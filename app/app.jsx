d16e05c58ffe117b7859544de0c930dcfc58c90b
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-05-31 13:23:30 -0700

    weathering

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Weather = require('Weather');
 
 ReactDOM.render(
   <Router history={hashHistory}>
