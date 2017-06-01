8f3f4b4d273daef5b4d2d88dd7a09fe187a12d49
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-01 12:12:29 -0700

    faking weather

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function() {
+    var {temp, location} = this.props;
     return (
-      <p>Weather Message</p>
+      <h3>It is {temp} and the location is {location}</h3>
     )
   }
 });
