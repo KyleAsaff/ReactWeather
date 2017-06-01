0178223c3e95b30a956d0cd51acf86051d0e0886
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-01 16:44:16 -0700

    port caps

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use(express.static('public'));
 
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' + port);
+  console.log('Express server is up on port ' + PORT);
 });
