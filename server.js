2ae42214c81c62ed2e77784c6f11341190620183
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-01 16:41:20 -0700

    prepping for heroku

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var cors = require('cors');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.header('x-forwarded-proto') === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + port);
 });
