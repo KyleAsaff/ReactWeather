b81eb2a7d1bf488c6bcc9ce328f4f5f89532cb75
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-02 11:51:22 -0700

    grid styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
+var {Link} = require('react-router');
 var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out:</p>
+    <ol>
+      <li>
+        <Link to='/?location=Halifax'>Halifax, NS</Link>
+      </li>
+      <li>
+        <Link to='/?location=Rio'>Rio, Brazil</Link>
+      </li>
+    </ol>
+  </div>
   )
 };
 
