a88d2617b8571e052ea804938775532c9b5795ac
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-03 11:57:20 -0700

    capitalize city

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,11 +2,13 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   var text = '';
+  var city = '';
   if(temp && location) {
-    text = `It is ${temp} and the location is ${location}`;
+    text = `It is currently ${temp}°F in`;
+    city = location;
   }
   return (
-    <h3>{text}</h3>
+    <h3>{text} <a className='city-name'>{city}</a></h3>
   )
 };
 
